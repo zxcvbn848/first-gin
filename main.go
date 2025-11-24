5615a0d8c4ec72e5f2d0b549504e388cf22c4253
andywang <andywang@run-cloud.com.tw>
2025-11-24 16:54:53 +0800

    feat: add album type

--- main.go
+++ main.go
@@ -8,6 +8,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and C", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
 func main() {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
